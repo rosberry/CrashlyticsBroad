9df59f80cbf2d56ab96982652bd17a4cc0077a90
nik <nikolay.tynin@rosberry.com>
2019-11-25 10:04:49 +0600

    remove firebase configuration from broad initializer

--- CrashlyticsBroad/CrashlyticsBroad.swift
+++ CrashlyticsBroad/CrashlyticsBroad.swift
@@ -1,32 +1,19 @@
 //
-//  CrashlyticsBroad.swift
-//  CrashlyticsBroad
+//  AppDelegate.swift
 //
-//  Created by Nick Tyunin on 22/08/2019.
 //  Copyright © 2019 Rosberry. All rights reserved.
 //
 
 import ButterBroad
-import Fabric
 import Firebase
-import Crashlytics
 
 final public class CrashlyticsBroad: ButterBroad.Analytics {
 
-    private var isFireBaseEnabled: Bool
-
-    public init(withFirebase isFireBaseEnabled: Bool = true) {
-        self.isFireBaseEnabled = isFireBaseEnabled
-        Fabric.with([Crashlytics.self])
-        if isFireBaseEnabled {
-            FirebaseApp.configure()
-        }
+    /// Creates an instance of CrashlyticsBroad. To use it one of the overrides of 'FirebaseApp.configure' methods should be called
+    public init() {
     }
 
     public func log(_ event: Event) {
-        Answers.logCustomEvent(withName: event.name, customAttributes: event.params)
-        if isFireBaseEnabled {
-            Firebase.Analytics.logEvent(event.name, parameters: event.params)
-        }
+        Firebase.Analytics.logEvent(event.name, parameters: event.params)
     }
 }
