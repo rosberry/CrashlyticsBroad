7a1527384093294b3be0d402d193f1053c7f8b2a
nik <nikolay.tynin@rosberry.com>
2020-06-11 11:18:12 +0600

    remove firebase app configuration

--- FirebaseBroad/FirebaseBroad.swift
+++ FirebaseBroad/FirebaseBroad.swift
@@ -9,17 +9,11 @@ import Firebase
 
 final public class FirebaseBroad: ButterBroad.Analytics {
 
-    private var needsConfigureApp: Bool
-
     public lazy var activationHandler: (() -> Void)? = {
-        if self.needsConfigureApp {
-            FirebaseApp.configure()
-        }
     }
 
     /// Creates an instance of FirebaseBroad. To use it one of the overrides of 'FirebaseApp.configure' methods should be called
-    public init(needsConfigureApp: Bool = false) {
-        self.needsConfigureApp = needsConfigureApp
+    public init() {
     }
 
     public func log(_ event: Event) {
